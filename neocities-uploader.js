1913fd6224323cd78ac520c5ab3475e756dd1ad0
nalmb <ntnwaslost@gmail.com>
2026-02-25 21:53:24 +0700

    Prompt Neocities uploader before upload

--- neocities-uploader.js
+++ neocities-uploader.js
@@ -1,15 +1,31 @@
 import NeoCities from "neocities";
 import fs from "fs"
 import path from "path"
+import readline from "readline";
 
 process.loadEnvFile("./development.env")
 
 const files = fs.readdirSync("./src", { recursive: true, withFileTypes: true }).filter(path => !path.isDirectory())
 const filesToUpload = files.map(file => ({ name: file.name, path: path.join(process.cwd(), file.parentPath, file.name) }))
 
+console.log("FILES TO UPLOAD")
+console.log(filesToUpload)
 
-const api = new NeoCities('cities-and-countries', process.env.NEOCITIES_PASSWORD)
+process.stdin.setRawMode(true);
+process.stdin.setEncoding("utf8")
+process.stdin.resume()
 
-api.upload(filesToUpload, function(resp) {
-  console.log(resp)
+console.log("Upload to neocities? Y/[N]")
+
+process.stdin.on("data", (input, a, b, c, d) => {
+  process.stdin.setRawMode(false)
+  process.stdin.pause()
+
+  if (input !== "y") return
+
+  const api = new NeoCities('cities-and-countries', process.env.NEOCITIES_PASSWORD)
+
+  api.upload(filesToUpload, function(resp) {
+    console.log(resp)
+  })
 })
